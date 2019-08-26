209fc76d1859fb83acb75a2cfdcde3f5e510b100
Rommel Azucena <jrommelazucena@gmail.com>
2019-08-26 22:14:35 +0800

    Conver the week widget to Statefulwidget
    To show the selected day on the current week.

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,8 @@ class MainScreen extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(vertical: 16.0),
                   child: CurrentWeekWidget(),
                 ),
-                Expanded(child: Padding(
+                Expanded(
+                    child: Padding(
                   padding: const EdgeInsets.only(top: 16.0),
                   child: DayViewWidget(),
                 ))
@@ -96,9 +97,49 @@ class WeekShortLabelWidget extends StatelessWidget {
   }
 }
 
-class CurrentWeekWidget extends StatelessWidget {
+class CurrentWeekWidget extends StatefulWidget {
+  @override
+  CurrentWeekWidgetState createState() => CurrentWeekWidgetState();
+}
+
+class CurrentWeekWidgetState extends State<CurrentWeekWidget> {
+  var selectedDayIndex = 0;
+
   List<String> _getDaysOfTheWeek() {
     return ['26', '27', '28', '29', '30', '31', '1'];
+  }
+
+  Widget buildDayWidget(
+      String dayOfTheWeek, Color textColor, int currentIndex) {
+    final isSelected = _getDaysOfTheWeek()[selectedDayIndex] == dayOfTheWeek;
+
+    final decoration = (isSelected)
+        ? BoxDecoration(
+          color: Color(0xFFFF8291),
+          shape: BoxShape.circle,
+          )
+        : null;
+
+    textColor = (isSelected) ? Colors.white : textColor;
+
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() => selectedDayIndex = currentIndex);
+        },
+        child: Container(
+          decoration: decoration,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              dayOfTheWeek,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: textColor, fontSize: 15),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
@@ -107,14 +148,8 @@ class CurrentWeekWidget extends StatelessWidget {
     List<Widget> list = new List<Widget>();
     for (var i = 0; i < daysOfTheWeek.length; i++) {
       final textColor = (i < 5) ? Colors.black : Color(0xFFA9AEB8);
-
-      list.add(Expanded(
-        child: Text(
-          daysOfTheWeek[i],
-          textAlign: TextAlign.center,
-          style: TextStyle(color: textColor, fontSize: 15),
-        ),
-      ));
+      list.add(buildDayWidget(
+          daysOfTheWeek[i], textColor, i));
     }
 
     return Row(children: list);
